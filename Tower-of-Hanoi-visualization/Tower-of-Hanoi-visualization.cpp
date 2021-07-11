cfe7894908b15aa17c6b9c354957ebaf200747d4
Mateusz Chmielewski <mateusz.chmielewski1@protonmail.com>
2021-07-12 00:20:16 +0200

    Creating the window

--- Tower-of-Hanoi-visualization/Tower-of-Hanoi-visualization.cpp
+++ Tower-of-Hanoi-visualization/Tower-of-Hanoi-visualization.cpp
@@ -6,5 +6,29 @@ using namespace sf;
 
 int main()
 {
+    //Creating the window
+    RenderWindow W;
+    W.create(VideoMode(1280, 720, 32), "Tower of Hanoi");
+    W.setActive(true);
+    W.setKeyRepeatEnabled(false);
+    W.setFramerateLimit(60);
+    W.setPosition(Vector2i(550, 50));
+    W.setVerticalSyncEnabled(true);
+
+
+    //Creating a event and display the window
+    Event event;
+
+    while (W.isOpen()) {
+        while (W.pollEvent(event))
+            if (event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape))
+                W.close();
+
+        W.clear(Color(255, 255, 255));
+
+        W.display();
+    }
+
+
     return 0;
 }
