dfda9e24223fc48efd803d7d837fa0298757862b
Mateusz Chmielewski <mateusz.chmielewski1@protonmail.com>
2021-07-12 10:47:21 +0200

    Creating and displaying shapes of pales

--- Tower-of-Hanoi-visualization/Tower-of-Hanoi-visualization.cpp
+++ Tower-of-Hanoi-visualization/Tower-of-Hanoi-visualization.cpp
@@ -7,26 +7,51 @@ using namespace sf;
 int main()
 {
     //Creating the window
-    RenderWindow W;
-    W.create(VideoMode(1280, 720, 32), "Tower of Hanoi");
-    W.setActive(true);
-    W.setKeyRepeatEnabled(false);
-    W.setFramerateLimit(60);
-    W.setPosition(Vector2i(550, 50));
-    W.setVerticalSyncEnabled(true);
+    RenderWindow displayedWindow;
+    displayedWindow.create(VideoMode(1280, 720, 32), "Tower of Hanoi");
+    displayedWindow.setActive(true);
+    displayedWindow.setKeyRepeatEnabled(false);
+    displayedWindow.setFramerateLimit(60);
+    displayedWindow.setPosition(Vector2i(550, 50));
+    displayedWindow.setVerticalSyncEnabled(true);
 
+    //Creating shapes of pales
+    Vector2f paleSize(40, 600);
+    auto palePositionX = displayedWindow.getSize().x / 4;
+    auto palePositionY = displayedWindow.getSize().y - paleSize.y;
 
-    //Creating a event and display the window
+    RectangleShape paleA(paleSize);
+    paleA.setFillColor(Color(255, 0, 0));
+    paleA.setOrigin(0, 0);
+    paleA.setPosition(palePositionX, palePositionY);
+
+    RectangleShape paleB(paleSize);
+    paleB.setFillColor(Color(0, 255, 0));
+    paleB.setOrigin(0, 0);
+    paleB.setPosition(palePositionX * 2, palePositionY);
+
+    RectangleShape paleC(paleSize);
+    paleC.setFillColor(Color(0, 0, 255));
+    paleC.setOrigin(0, 0);
+    paleC.setPosition(palePositionX * 3, palePositionY);
+
+    //Creating a event and displaying the window
     Event event;
 
-    while (W.isOpen()) {
-        while (W.pollEvent(event))
+    while (displayedWindow.isOpen()) {
+        while (displayedWindow.pollEvent(event))
             if (event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape))
-                W.close();
+                displayedWindow.close();
 
-        W.clear(Color(255, 255, 255));
+        displayedWindow.clear(Color(255, 255, 255));
 
-        W.display();
+        //Dislaing pales
+        displayedWindow.draw(paleA);
+        displayedWindow.draw(paleB);
+        displayedWindow.draw(paleC);
+
+
+        displayedWindow.display();
     }
 
 
